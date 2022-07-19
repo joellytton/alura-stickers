41a9360dffa5ac3f7ea0408578d9e46c83a358b5
joellytton <joellytton@pop-os.localdomain>
2022-07-19 14:46:32 -0500

    aula 01

---
+++ src/App.java
@@ -0,0 +1,32 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
+
+public class App {
+    private static HttpClient newHttpClient;
+
+    public static void main(String[] args) throws Exception {
+        // fazer uma conexão http e buscar os top 250 filmes
+        String url = "https://api.mocki.io/v2/549a5d8b";
+        URI endereco = URI.create(url);
+        var client = HttpClient.newHttpClient();
+        var request = HttpRequest.newBuilder(endereco).GET().build();
+        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+        String body = response.body();
+        var parser = new JsonParser();
+
+        // extrair só os dados que interessam (titulo, poster, classificação)
+        List<Map<String, String>> listaDeFilmes = parser.parse(body);
+        
+        // exibir e manipular os dados
+        for (Map<String,String> filme : listaDeFilmes) {
+            System.out.println(filme.get("title"));
+            System.out.println(filme.get("image"));
+            System.out.println(filme.get("imDbRating"));
+        }
+    }
+}
